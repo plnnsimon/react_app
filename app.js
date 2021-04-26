c7ee414c91d3b4c2942df231fe4e029fbb1059ae
Олександр Пальонний <s2567k@gmail.com>
2021-04-26 09:48:28 +0300

    commit#4 react_app

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes.js'))
 
+app.use('/api/link', require('./routes/link.routes.js'))
+
 const PORT = config.get('port') || 5000
 
 async function start () {
